9f44fe6ff7602a6a05bbcdaa2424ce351175e7c8
Aditya Gupta <adi.h4ck3r@gmail.com>
2020-10-30 12:57:14 +0530

    Update PyAssist.py

--- PyAssist.py
+++ PyAssist.py
@@ -24,62 +24,6 @@ def SpeakText(command):
     engine.say(command)  
     engine.runAndWait() 
       
-#Initialization
-def init():
-    SpeakText('hello, i am Kevin, at you service.')
-    NameAuth()
-
-#Name Authentication
-def NameAuth():
-    while(1):     
-      
-        # Exception handling to handle 
-        # exceptions at the runtime 
-        try: 
-          
-            # use the microphone as source for input. 
-            with sr.Microphone() as source2: 
-                SpeakText('Initializing the Authentication')
-                SpeakText('identify yourself')
-
-                # wait for a second to let the recognizer 
-                # adjust the energy threshold based on 
-                # the surrounding noise level  
-                r.adjust_for_ambient_noise(source2, duration=0.2) 
-            
-                #Authenticating user
-                #auth = 0
-                #while auth != 1:
-                SpeakText('Your Name Please')
-                audio1 = r.listen(source2) 
-                MyText1 = r.recognize_google(audio1)
-                MyText1 = MyText1.lower()
-                SpeakText('You Name: '+MyText1)
-                if MyText1 == 'aditya':
-                    SpeakText('hello aditya, welcome')
-                    SpeakText('Get Ready for image authentication')
-                    #ImageAuthentication()
-                    print('Starting the Face Recognition. Please Wait...')
-                    auth = FaceRecognition()
-                    print('Face Recognition Completed')
-                    if auth:
-                        SpeakText('Verified')
-                        print('Verified')
-                        OpenApp()
-                    else: 
-                        print('Face not Matched')
-                        SpeakText('Face did not match')
-                else:
-                    SpeakText('Wrong Password, Try again')
-
-        except sr.RequestError as e: 
-            print("Could not request results; {0}".format(e)) 
-            SpeakText('Could not Recognize, Try again')
-          
-        except sr.UnknownValueError: 
-            print("unknown error occured") 
-            SpeakText('Could not Recognize, Try again')
-
 #Function to capture the image for image authentication
 def ImageAuthentication():
     SpeakText('Capturing the picture')
@@ -188,6 +132,105 @@ def FaceRecognition():
 
 # Loop infinitely for user to 
 # speak 
-speaking = True
-while speaking:
-    init()
+SpeakText('hello, i am Kevin, at you service.')
+while(1):     
+      
+    # Exception handling to handle 
+    # exceptions at the runtime 
+    try: 
+          
+        # use the microphone as source for input. 
+        with sr.Microphone() as source2: 
+            SpeakText('Initializing the Authentication')
+            SpeakText('identify yourself')
+
+            # wait for a second to let the recognizer 
+            # adjust the energy threshold based on 
+            # the surrounding noise level  
+            r.adjust_for_ambient_noise(source2, duration=0.2) 
+            
+            #Authenticating user
+            #auth = 0
+            #while auth != 1:
+            SpeakText('Your Name Please')
+            audio1 = r.listen(source2) 
+            MyText1 = r.recognize_google(audio1)
+            MyText1 = MyText1.lower()
+            SpeakText('You Name: '+MyText1)
+            if MyText1 == 'aditya':
+                SpeakText('hello aditya, welcome')
+                SpeakText('Get Ready for image authentication')
+                #ImageAuthentication()
+                print('Starting the Face Recognition. Please Wait...')
+                auth = FaceRecognition()
+                print('Face Recognition Completed')
+                if auth:
+                    SpeakText('Verified')
+                    print('Verified')
+                    OpenApp()
+                else: 
+                    print('Face not Matched')
+                    SpeakText('Face did not match')
+            else:
+                SpeakText('Wrong Password, Try again')
+
+
+            #SpeakText('Welcome to the world of Jarvis')
+
+            # #listens for the user's input  
+            # SpeakText('how may i help you')
+            # audio2 = r.listen(source2) 
+              
+            # # Using ggogle to recognize audio 
+            # MyText = r.recognize_google(audio2) 
+            # MyText = MyText.lower() 
+  
+            # print("Did you say "+MyText) 
+            # SpeakText(MyText) 
+
+            # #if Sentence consists of open --name of the app-- 
+            # if('open' in MyText):
+            #     print("Openning the App, Please Wait...")
+            #     myapp = MyText.split('open')[1]
+            #     print(myapp)
+            #     if myapp==' command prompt':
+            #         myapp = 'cmd'
+            #     elif myapp==' mozilla firefox':
+            #         myapp = 'firefox'
+            #         os.system(myapp)
+                
+        #Continuing after authentication
+        # with sr.Microphone() as source3:
+
+        #     SpeakText('Which application do you want to open aditya')
+            
+        #     r.adjust_for_ambient_noise(source2, duration=0.2)
+
+        #     audio3 = r.listen(source3) 
+              
+        #     # Using ggogle to recognize audio 
+        #     MyText3 = r.recognize_google(audio3) 
+        #     MyText3 = MyText3.lower() 
+  
+        #     print("Did you say "+MyText3) 
+        #     SpeakText(MyText3) 
+
+        #     #if Sentence consists of open --name of the app-- 
+        #     if('open' in MyText3):
+        #         print("Openning the App, Please Wait...")
+        #         myapp = MyText3.split('open')[1]
+        #         print(myapp)
+        #         if myapp==' command prompt':
+        #             myapp = 'cmd'
+        #         elif myapp==' mozilla firefox':
+        #             myapp = 'firefox'
+        #             os.system(myapp)
+
+              
+    except sr.RequestError as e: 
+        print("Could not request results; {0}".format(e)) 
+          
+    except sr.UnknownValueError: 
+        print("unknown error occured") 
+
+    
